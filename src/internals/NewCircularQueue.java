3b0955fb57c42ca22c2f32c3d9927fc748f5e7d9
oquzhax <47812929+oquzhax@users.noreply.github.com>
2019-03-28 20:32:41 +0300

    ReWrited circular queue
    i hope this time it will work

---
+++ src/internals/NewCircularQueue.java
@@ -0,0 +1,94 @@
+package internals;
+
+public class NewCircularQueue<T> implements IQueue<T> {
+
+	private final static int DEFAULT_SIZE = 10;
+	private final static int MAX_SIZE = 10000;
+	private int size;
+	T[] queue;
+	private int frontIndex = 0;
+	private int backIndex = -1;
+	
+	
+	
+	public NewCircularQueue() {
+		this(DEFAULT_SIZE);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public NewCircularQueue(int size) {
+		queue = (T[]) new Object[size];
+	}
+
+	private void ensureCapacity() {
+		if(size == MAX_SIZE)
+		{
+			throw new IllegalStateException("Reached max size");
+		}
+		else if((backIndex + 2) % queue.length == frontIndex % queue.length) 
+		{
+			@SuppressWarnings("unchecked")
+			T[] newQueue = (T[]) new Object[size + 10];
+			for(int i = 0; i < queue.length; i++) 
+			{
+				newQueue[i] = queue[i];
+			}
+			queue = newQueue;
+		}
+	}
+	
+	
+	@Override
+	public void enQueue(T newEntry) {
+		ensureCapacity();
+		backIndex = (backIndex + 1) % queue.length;
+		queue[backIndex] = newEntry;
+
+		
+	}
+
+	@Override
+	public T deQueue() {
+		if(!isEmpty()) 
+		{
+			T removedItem = queue[frontIndex];
+			queue[frontIndex] = null;
+			if(queue[(frontIndex + 1) % queue.length] != null)
+			{
+				frontIndex = (frontIndex + 1) % queue.length;
+			}
+			return removedItem;
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	@Override
+	public T getFront() {
+		T frontEntry = queue[frontIndex];
+		return frontEntry;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		if(queue[frontIndex] == null)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	@Override
+	public void clear() {
+		while(frontIndex != backIndex)
+		{
+			deQueue();
+		}
+	}
+
+}
---
+++ src/unitTests/NewCircularQueueTest.java
@@ -0,0 +1,91 @@
+package unitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import internals.ArrayQueue;
+import internals.NewCircularQueue;
+import parts.*;
+
+class NewCircularQueueTest {
+
+	private NewCircularQueue<IProduct> queue;
+	private IProduct ram; IProduct cpu; IProduct cache; IProduct GraphicsCard; IProduct Motherboard;
+	
+
+	@BeforeEach
+	void setUp() throws Exception {
+		queue = new NewCircularQueue<IProduct>();
+		ram = new RAM();
+		cpu = new CPU();
+		cache = new Cache();
+		GraphicsCard = new GraphicsCard();
+		Motherboard = new parts.Motherboard();
+		
+	}
+
+	@Test
+	void testArrayQueue() {
+	}
+
+	@Test
+	void testEnQueue() {
+		/*for(int i = 0; i < 100; i++) {
+			queue.enQueue(ram);
+		} */
+		ArrayQueue<String> que = new ArrayQueue<String>();
+		for (int i = 0; i < 100; i++) {
+			que.enQueue(((Integer) i).toString());
+		}
+	}
+
+	@Test
+	void testDeQueue() {
+/*		assertEquals(null, queue.deQueue());
+		queue.enQueue(ram);
+		queue.enQueue(cache);
+		queue.enQueue(cpu);
+		assertEquals(ram, queue.deQueue());
+		assertEquals(cache, queue.deQueue());
+		assertEquals(cpu, queue.deQueue());
+		assertEquals(null, queue.deQueue()); */
+		ArrayQueue<String> lolcat = new ArrayQueue<String>();
+		for (int i = 0; i < 9; i++) {
+			lolcat.enQueue(((Integer)i).toString());
+		}
+		for (int j = 0; j < 4; j++) {
+			lolcat.deQueue();
+		}
+		for (int k = 10; k < 40; k++)
+			lolcat.enQueue(((Integer) k).toString());
+	}
+
+	@Test
+	void testGetFront() {
+		assertEquals(null, queue.getFront());
+		queue.enQueue(ram);
+		queue.enQueue(cache);
+		queue.enQueue(cpu);
+		assertEquals(ram, queue.getFront());
+	}
+
+	@Test
+	void testIsEmpty() {
+		assertEquals(true, queue.isEmpty());
+		queue.enQueue(ram);
+		assertEquals(false, queue.isEmpty());
+	}
+
+	@Test
+	void testClear() {
+		queue.enQueue(ram);
+		queue.enQueue(cache);
+		queue.enQueue(cpu);
+		assertEquals(false, queue.isEmpty());
+		queue.clear();
+		assertEquals(true, queue.isEmpty());
+	}
+
+}
